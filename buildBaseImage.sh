263fc97dcc4b0191da51591665e7b09de33bc6e0
Mike Holczer <github@holczer.com>
2015-03-15 17:01:29 +0000

    adding script to build pdcbase docker image

---
+++ buildBaseImage.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+### Build a docker image for debian  armhf
+### based on https://github.com/docker-32bit/debian
+
+### settings
+arch=armhf
+suite=jessie
+chroot_dir='/var/chroot/jessie'
+apt_mirror='http://http.debian.net/debian'
+docker_image='pdcbase:latest'
+
+### make sure that the required tools are installed
+apt-get install -y docker.io debootstrap dchroot
+
+### install a minbase system with debootstrap
+export DEBIAN_FRONTEND=noninteractive
+debootstrap --arch $arch $suite $chroot_dir $apt_mirror
+
+### update the list of package sources
+cat <<EOF > $chroot_dir/etc/apt/sources.list
+deb $apt_mirror jessie main contrib non-free
+deb $apt_mirror jessie-updates main contrib non-free
+deb http://security.debian.org/ jessie/updates main contrib non-free
+EOF
+
+### cleanup
+chroot $chroot_dir apt-get autoclean
+chroot $chroot_dir apt-get clean
+chroot $chroot_dir apt-get autoremove
+
+### create a tar archive from the chroot directory
+tar cfz debian.tgz -C $chroot_dir .
+
+### import this tar archive into a docker image:
+cat debian.tgz | docker import - $docker_image
+
+# ### cleanup
+rm debian.tgz
+rm -rf $chroot_dir
