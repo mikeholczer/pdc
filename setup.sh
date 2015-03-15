5c14887ec73d911aaa0f7aa7f7ffe48120e7b29d
Mike Holczer <github@holczer.com>
2015-03-15 21:44:39 +0000

    Adding Dockerfile to build python image and updated setup.sh to build it and the base image

--- setup.sh
+++ setup.sh
@@ -4,4 +4,14 @@
 apt-get update && apt-get dist-upgrade -y
 
 ## Install some additional software
-apt-get install git docker.io -y
+apt-get install git -y
+
+## Get the rest of the code
+git clone https://github.com/mikeholczer/pdc.git
+cd pdc
+
+## Build the base image
+./buildBaseImage.sh
+
+## Build some utility images
+docker build -t pdcpython DockerFiles/Python/
