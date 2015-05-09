fc1beb97e1a393197394046eb28b0dd14c7b3701
Mike Holczer <github@holczer.com>
2015-05-09 14:56:43 -0400

    removing old docker related files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Build the base image
-./buildBaseImage.sh
-
-## Build some utility images
-docker build -t pdcnode DockerFiles/Node/
-docker build -t pdcnatman DockerFiles/NatMan/
-
